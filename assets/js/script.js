8658367695a5895c1e5828d665c03330ce5733c8
robyng <robyngraham01@gmail.com>
2021-10-29 09:05:52 -0700

    push new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+    } else {
+    alert("Please enter a GitHub username");
+    }
+
+    console.log(event);
   };
-  getUserRepos();
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+          console.log(data);
+        });
+      });
+}
+userFormEl.addEventListener("submit", formSubmitHandler);
